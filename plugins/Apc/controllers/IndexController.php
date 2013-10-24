3922f61c48476ae5e4b6706399f30d9113cc0a3b
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2013-10-24 14:57:37 +0200

    apc and settings

--- plugins/Apc/controllers/IndexController.php
+++ plugins/Apc/controllers/IndexController.php
@@ -6,9 +6,9 @@ class Apc_IndexController extends Omeka_Controller_AbstractActionController
             apc_clear_cache();
             apc_clear_cache('user');
             apc_clear_cache('opcode');
-            echo json_encode(array('APC Clear Cache' => true));
+            //echo json_encode(array('APC Clear Cache' => true));
             //}
-            exit;
+            //exit;
         }
 
 }?>
