c6d0b88bfe16ef3dccdf0fc5986b13feda7d6944
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2014-01-06 11:17:03 +0100

    fix #43

--- themes/default/index.php
+++ themes/default/index.php
@@ -23,7 +23,7 @@
                     <div class="label"><?php echo __("Deze pagina: ")?></div>
                    <ul>
                         <li class="first"><a href="#" onClick="window.print()" class="page print"><?php echo __("printen")?></a></li>
-                        <li><li><a class="addthis_button page share" " href="http://www.addthis.com/bookmark.php"><?php echo __("delen")?></a></li>
+                        <li><li><a class="addthis_button page share" href="http://www.addthis.com/bookmark.php"><?php echo __("delen")?></a></li>
                     </ul>
 		</div>
             </div>
--- themes/default/items/browse.php
+++ themes/default/items/browse.php
@@ -78,7 +78,7 @@ echo head(array('title'=>$pageTitle,'bodyclass' => 'items browse'));
                     <div class="label"><?php echo __("Deze pagina: ")?></div>
                     <ul>
                         <li class="first"><a href="#" onClick="window.print()" class="page print"><?php echo __("printen")?></a></li>
-                        <li><li><a class="addthis_button page share" " href="http://www.addthis.com/bookmark.php"><?php echo __("delen")?></a></li>
+                        <li><li><a class="addthis_button page share" href="http://www.addthis.com/bookmark.php"><?php echo __("delen")?></a></li>
                     </ul>
 		</div>
             </div>
