696c24f18354a9e3393e2bb23100428f4a368176
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2014-09-04 13:49:44 +0200

    #57 IE caching-probleem

--- themes/default/common/header.php
+++ themes/default/common/header.php
@@ -1,14 +1,15 @@
-<!DOCTYPE html>
-<html lang="<?php echo get_html_lang(); ?>">
-<head>
-    <?php
+ <?php
     header("Content-Type: application/json");
     header("Expires: on, 01 Jan 1970 00:00:00 GMT");
     header("Last-Modified: " . gmdate("D, d M Y H:i:s") . " GMT");
     header("Cache-Control: no-store, no-cache, must-revalidate");
     header("Cache-Control: post-check=0, pre-check=0", false);
     header("Pragma: no-cache");
-    ?>
+?>
+<!DOCTYPE html>
+<html lang="<?php echo get_html_lang(); ?>">
+<head>
+   
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <?php if ( $description = option('description')): ?>
