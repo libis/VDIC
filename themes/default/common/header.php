ba551d011b9c1cbac37450255a8bdc81583eb3cd
JorisLambrechts <joris.lambrechts@bib.kuleuven.be>
2014-09-04 14:14:35 +0200

    #57 IE caching-probleem

--- themes/default/common/header.php
+++ themes/default/common/header.php
@@ -1,7 +1,6 @@
- <?php
-    header("Expires: on, 01 Jan 1970 00:00:00 GMT");
-    header("Cache-Control: no-store, no-cache, must-revalidate, post-check=0, pre-check=0");
-    header("Pragma: no-cache");
+<?php
+    header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
+    header("Expires: Sat, 26 Jul 1997 05:00:00 GMT"); // Date in the past
 ?>
 <!DOCTYPE html>
 <html lang="<?php echo get_html_lang(); ?>">
