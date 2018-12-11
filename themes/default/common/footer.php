2c9e7b4f958aa77c1424bc28d63ff1bc9ec73ba3
Joris <joris.lambrechts@kuleuven.be>
2018-12-11 12:39:04 +0100

    cookies

--- themes/default/common/footer.php
+++ themes/default/common/footer.php
@@ -8,6 +8,7 @@
                     <li class="first"><span class="page"><span class="copyright">© 2013</span> - <?php echo __('FOD Volksgezondheid, Veilighed van de Voedselketen en Leefmilieu');?></span></li>
                     <li><a href="<?php echo url(libis_get_language_slug()."about/regulations")?>" class="page"><?php echo __("Juridische informatie")?></a></li>
                     <li class="last active"><a href="<?php echo url(libis_get_language_slug()."about/privacy")?>" class="page"><?php echo __('Privacy');?></a></li>
+                    <li class="last active"><a href="<?php echo url(libis_get_language_slug()."about/cookies")?>" class="page"><?php echo __('Cookies');?></a></li>
                 </ul>
             </div>
             <?php fire_plugin_hook('public_footer'); ?>
