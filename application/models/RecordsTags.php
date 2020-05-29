1df0ce838895a2ba9bb1bac2cfb8010c41720100
Joris <joris.lambrechts@kuleuven.be>
2020-05-29 13:07:36 +0200

    fix tags

--- application/models/RecordsTags.php
+++ application/models/RecordsTags.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Omeka
- * 
+ *
  * @copyright Copyright 2007-2012 Roy Rosenzweig Center for History and New Media
  * @license http://www.gnu.org/licenses/gpl-3.0.txt GNU GPLv3
  */
 
 /**
  * Linkage between a record and a tag.
- * 
+ *
  * @package Omeka\Record
  */
 class RecordsTags extends Omeka_Record_AbstractRecord
@@ -40,4 +40,9 @@ class RecordsTags extends Omeka_Record_AbstractRecord
      * @var string
      */
     public $time;
+
+    protected function _initializeMixins()
+    {
+        $this->_mixins[] = new Mixin_Timestamp($this, 'time', null);
+    }
 }
